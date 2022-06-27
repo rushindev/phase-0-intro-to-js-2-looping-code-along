8c35f4625d19b5047ae4820e05dc79929f762b45
York Rushin <rushinyork@gmail.com>
2022-06-27 06:13:44 -0700

    commit tree c657ee7514ad1ba0f15ab22430851f360de6a4be

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const person = [];
+const msgCard = [];
+let msg
+let cardMsg
+
+function writeCards(person, msg) {
+    for (let i = 0; i < person.length; i++) {
+        cardMsg = `Thank you, ${person[i]}, for the wonderful ${msg} gift!`;
+        msgCard.push(cardMsg);
+    }
+
+    return msgCard;
+
+}
+
+function countDown() {
+    let down = 10;
+    while (down >= 0) {
+        console.log(down--);
+    }
+}
